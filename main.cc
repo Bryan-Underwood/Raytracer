841ea3050160ef37a2260e459018fe013d054c37
Bryan-Underwood <btu@gmail.com>
2026-08-18 16:20:36 -0500

    introduced shading with surface normals

--- main.cc
+++ main.cc
@@ -5,18 +5,33 @@
 #include "color.h"
 #include "ray.h"
 
-bool hit_sphere(const point3& center, double radius, const ray& r) {
+double hit_sphere(const point3& center, double radius, const ray& r) {
+    //Ray starting from camera going to sphere center
     vec3 oc = center - r.origin();
-    auto a = dot(r.direction(), r.direction());
-    auto b = -2. * dot(r.direction(), oc);
-    auto c = dot(oc, oc) - radius*radius;
-    auto discriminant = b*b - 4*a*c;
-    return (discriminant >= 0);
+
+    //Coefficients of the quadratic in t
+    auto a = r.direction().length_squared();
+    auto h = dot(r.direction(), oc);
+    auto c = oc.length_squared() - radius*radius;
+    auto discriminant = h*h - a*c;
+
+    if (discriminant < 0) 
+        return -1.;
+    
+    else
+        //Solve quadratic for t, the scalar value of the ray when it intersects the sphere
+        return (h - std::sqrt(discriminant)) / a;
+    
 }
+
 color ray_color(const ray& r) { 
 
-    if (hit_sphere(point3(0,0,-1), .5, r))
-        return color(1,0,0);
+    auto t = hit_sphere(point3(0,0,-1), 0.5, r);
+    if (t > 0.0) {
+        //Surface normal = surface point - center point
+        vec3 N = unit_vector(r.at(t) - vec3(0,0,-1));
+        return 0.5 * color(N.x()+1, N.y()+1, N.z()+1);
+    }
 
     //Normalize to hold within [-1,1] values
     vec3 unit_direction = unit_vector(r.direction());
--- ray.h
+++ ray.h
@@ -12,6 +12,8 @@ class ray {
         const point3& origin() const { return orig; }
         const vec3& direction() const {return dir; }
 
+
+        //Return point at certain postion on the ray
         point3 at(double t) const {
             return orig + t * dir;
         }
