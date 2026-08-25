73b3b79f2bc547d030c5c4b5a14375e73f8e60e6
Bryan-Underwood <btu@gmail.com>
2026-08-24 21:39:51 -0500

    Add lambertian reflection to diffuse material

--- camera.h
+++ camera.h
@@ -115,7 +115,9 @@ class camera {
             hit_record rec;
 
             if (world.hit(r, interval(0.001, infinity), rec)) {
-                vec3 direction = random_on_hemisphere(rec.normal);
+                
+                //Lambertian Reflection
+                vec3 direction = rec.normal + random_unit_vector();
                 return 0.5 * ray_color(ray(rec.p, direction), depth -1, world);
             }
 
